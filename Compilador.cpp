86559f38142fcdfc6fe8d03ae00c065802444549
Adrian Rivera Trujillo <adrianriveratrujillocetis154@gmail.com>
2021-10-13 11:07:32 -0500

    Agrege el cpp

---
+++ Compilador.cpp
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <math.h>
+#include <fstream>
+#include <windows.h>
+void Analisis_Lexico();
+void Analisis_Sintactico();
+void Analisis_Semantico();
+void Generacion_de_codigo();
+void Optimosacon();
+void Generacion_de_codigo_objeto();
+void Errores();
+void Tabla_de_simbolos();
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
